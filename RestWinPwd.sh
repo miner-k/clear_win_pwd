eec357abbad44fce05e7e11d6fc7486fd642e507
root <root@linux-zyk.novalocal>
2017-12-08 17:36:10 +0800

    restpasswd for huaweiclouds linux ECS

---
+++ RestWinPwd.sh
@@ -0,0 +1,4 @@
+wget http://cn-south-1-cloud-reset-pwd.obs.myhwclouds.com/linux/64/reset_pwd_agent/CloudResetPwdAgent.zip
+unzip CloudResetPwdAgent.zip
+cd CloudResetPwdAgent/CloudResetPwdUpdateAgent.Linux/
+bash setup.sh
